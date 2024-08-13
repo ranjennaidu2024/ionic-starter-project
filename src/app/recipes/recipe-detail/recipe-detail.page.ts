60981166289d2e2341161789ae3e7b4288eb4077
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-13 16:34:26 +0800

    Using Angular Features on Ionic Components

---
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.page.html',
+  styleUrls: ['./recipe-detail.page.scss'],
+})
+export class RecipeDetailPage {
+  constructor() {}
+}
